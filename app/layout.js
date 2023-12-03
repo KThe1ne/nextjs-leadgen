190ba134b045a28065d1035ba9646c0aa3439b1a
KThe1ne <83618130+KThe1ne@users.noreply.github.com>
2023-12-03 01:01:49 -0500

    FIxed Google Tag - dangerouslySetInnerHTML

--- app/layout.js
+++ app/layout.js
@@ -15,14 +15,19 @@ export default function RootLayout({ children }) {
     <html lang="en">
       {/* <link rel="icon" href="/favicon.ico" sizes="any" /> */}
       <Head>
-        <Script id="google-tag-manager" strategy='afterInteractive'>{`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-544GVJ2Z');
-        `}
-        </Script>
+        <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-544GVJ2Z');
+          `,
+        }}
+      />
       </Head>
       <body className={montserrat.className}>
         {/* Google Tag Manager (noscript) */}
