d87fefa5a569d566e59d9a302a4c9a5ddbbca437
KThe1ne <83618130+KThe1ne@users.noreply.github.com>
2023-12-02 22:44:50 -0500

    Added GTM Tag

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Montserrat } from 'next/font/google'
+import Head from 'next/head'
+import Script from 'next/script'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -12,7 +14,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/* <link rel="icon" href="/favicon.ico" sizes="any" /> */}
-      <body className={montserrat.className}>{children}</body>
+      <Head>
+        <Script id="google-tag-manager" strategy='afterInteractive'>{`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-544GVJ2Z');
+        `}
+        </Script>
+      </Head>
+      <body className={montserrat.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-544GVJ2Z"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {children}
+        {/* End Google Tag Manager (noscript) */}
+      </body>
     </html>
   )
 }
