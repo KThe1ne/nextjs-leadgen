9ebe327cf9640032766d3f0feb5462eb9abd04dd
KThe1ne <83618130+KThe1ne@users.noreply.github.com>
2024-02-16 10:10:11 -0500

    Added parse-input endpoint

--- app/api/send-pdf/create-pdf/route.js
+++ app/api/send-pdf/create-pdf/route.js
@@ -1,5 +1,26 @@
 const { NextRequest, NextResponse } = require("next/server");
-import { leadGenPDF } from "./createPDF.js";
+// import { leadGenPDF } from "./createPDF.js";
+import { jsPDF } from "jspdf";
+
+const leadGenPDF = async ({ username, magnetIdeas }) => {
+    return new Promise (async (resolve, reject) => {
+        const doc = new jsPDF("p", "px", "a4");
+        doc.addFont()
+        const width = doc.internal.pageSize.getWidth()
+        const height = doc.internal.pageSize.getHeight()
+        const imgData = fs.readFileSync("./public/cover-page.png");
+        const imgAsBase64 = Buffer.from(imgData).toString('base64');
+        
+        // const doc = new PDFDocument({ size: 'A4', font: "https://leadgen.diemmo.com/fonts/Montserrat-Regular.otf" });
+
+        doc.addImage(imgAsBase64, 0, 0, width, height)
+        doc.addPage()
+
+
+        doc.save(`${username}.pdf`)
+        return resolve("Success")
+    })
+};
 
 export async function POST(req) {
 
@@ -17,6 +38,6 @@ export async function POST(req) {
 
         console.error("Error creating PDF:", error);
         return NextResponse.json({ error: 'PDF creation failed' }, { status: 400 });
-        
+
     }
 }
