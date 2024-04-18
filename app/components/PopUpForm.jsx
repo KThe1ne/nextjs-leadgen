0c72eb6354e68c18eab617f078ebdb9779a43932
KThe1ne <83618130+KThe1ne@users.noreply.github.com>
2024-04-17 20:21:21 -0500

    Upsert Fix

--- app/components/PopUpForm.jsx
+++ app/components/PopUpForm.jsx
@@ -49,7 +49,6 @@ const PopUpForm = ({ setIsLeadInfoGiven, isLeadInfoGiven, setLeadDetails=false }
 		})
 		.then((res) => res.json())
 		.then((res) => {
-			console.log(res)
 			data["userId"] = res?.contact["id"];
 			if (setLeadDetails !== false) {
 				console.log(data)
